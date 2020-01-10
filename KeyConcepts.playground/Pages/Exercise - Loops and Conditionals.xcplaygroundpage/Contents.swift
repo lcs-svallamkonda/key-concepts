edd539aeeccd7053d4b980df3d1becd7095eedb4
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2020-01-10 12:15:47 -0500

    created fr in loop to create coloured rectangles for sloan poster. and created sloan text

--- KeyConcepts.playground/Pages/Exercise - Loops and Conditionals.xcplaygroundpage/Contents.swift
+++ KeyConcepts.playground/Pages/Exercise - Loops and Conditionals.xcplaygroundpage/Contents.swift
@@ -38,6 +38,33 @@ let neonGreen = Color(hue: 107, saturation: 61, brightness: 93, alpha: 100)
 // HINT: Remember that all shapes have a fill and a border.
 //       You can turn off the fill or border if desired.
 
+//create a rectangle to cover the background
+canvas.drawShapesWithBorders = false
+canvas.fillColor = deepOrange
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+
+//create alternating blue and green rectangles
+for counter in 0...7 {
+    
+    //determine x and y position
+    let x = counter * 50
+    var y = 0
+    
+    //set fill color and y position
+    let remainder = x % 100
+    if remainder == 0 {
+        canvas.fillColor = lightBlue
+    } else {
+        canvas.fillColor = neonGreen; y = 50
+    }
+    //draw the rectangles
+    canvas.drawRectangle(at: Point(x: x, y: y), width: 50, height: 350)
+}
+
+//draw "sloan" text
+canvas.textColor = Color.white
+canvas.drawText(message: "sloan", at: Point(x: 150, y: 400), size: 90, kerning: 3)
+
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
